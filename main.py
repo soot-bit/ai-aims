773df36f4df2667d43a78ff28c97e7e674471ca6
oepeng <84843604+soot-bit@users.noreply.github.com>
2024-05-06 01:11:59 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,9 +3,4 @@ import numpy as np
 
 filename = "./data/tutorial.csv"
 df = pd.read_csv(filename)
-print(df.head())
-x = df['x'].to_numpy()
-y = df['y'].to_numpy()
-print(len(x))  # print number of elements
-
-print("Hello world!")
+# to be finalised
